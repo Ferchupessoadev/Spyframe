85cf6b56ad4c80069df68c999fe0ae450505f926
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-10-04 17:06:57 -0300

    Create Routes PUT and DELETE

--- src/lib/Route.php
+++ src/lib/Route.php
@@ -32,134 +32,159 @@ namespace Spyframe\lib;
  */
 class Route
 {
-    /*
+	/*
      * * routes
      * @var array
      */
-    private static $routes = [
-        'GET' => [],
-        'POST' => [],
-        'PUT' => [],
-        'DELETE' => []
-    ];
+	private static $routes = [
+		'GET' => [],
+		'POST' => [],
+		'PUT' => [],
+		'DELETE' => []
+	];
 
-    protected static $request;
+	protected static $request;
 
-    /*
+	/*
      * * create route GET
      * @param $uri
      * @param $callback
      */
 
-    public static function get($uri, $callback): void
-    {
-        $uri = trim($uri, '/');
-        self::$routes['GET'][$uri] = $callback;
-    }
+	public static function get($uri, $callback): void
+	{
+		$uri = trim($uri, '/');
+		self::$routes['GET'][$uri] = $callback;
+	}
 
-    /*
+	/*
      * * create route POST
      * @param $uri
      * @param $callback
      */
-    public static function post($uri, $callback): void
-    {
-        $uri = trim($uri, '/');
-        self::$routes['POST'][$uri] = $callback;
-    }
+	public static function post($uri, $callback): void
+	{
+		$uri = trim($uri, '/');
+		self::$routes['POST'][$uri] = $callback;
+	}
 
-    /*
+	/**
+	 * * create route PUT
+	 * @param $uri
+	 * @param $callback
+	 * @return void 
+	 */
+	public static function put($uri, $callback): void
+	{
+		$uri = trim($uri, '/');
+		self::$routes['PUT'][$uri] = $callback;
+	}
+
+
+	/**
+	 * * create route DELETE
+	 * @param $uri
+	 * @param $callback
+	 * @return void 
+	 */
+	public static function delete($uri, $callback): void
+	{
+		$uri = trim($uri, '/');
+		self::$routes['DELETE'][$uri] = $callback;
+	}
+
+	/*
      * * Response
      * @param $response
      * @param int $statusCode
      */
-    public static function Response($response, $statusCode = 200): void
-    {
-        http_response_code($statusCode);
-        if (is_array($response) || is_object($response)) {
-            header('Content-Type: application/json');
-            echo json_encode($response);
-        } else {
-            echo $response;
-        }
-    }
+	public static function Response($response, $statusCode = 200): void
+	{
+		http_response_code($statusCode);
+		if (is_array($response) || is_object($response)) {
+			header('Content-Type: application/json');
+			echo json_encode($response);
+		} else {
+			echo $response;
+		}
+	}
 
-    /*
+	/*
      * * start route
      */
-    public static function start(): void
-    {
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-        $uri = trim($uri, '/');
-        $uri = explode('?', $uri)[0];
+	public static function start(): void
+	{
+		$uri = $_SERVER['REQUEST_URI'];
+		$method = $_SERVER['REQUEST_METHOD'];
+		$uri = trim($uri, '/');
+		$uri = explode('?', $uri)[0];
 
-        // validate method
-        if (!isset(self::$routes[$method])) {
-            http_response_code(405);
-            self::Response(['message' => 'Method Not Allowed']);
-            return;
-        }
+		// validate method
+		if (!isset(self::$routes[$method])) {
+			http_response_code(405);
+			self::Response(['message' => 'Method Not Allowed']);
+			return;
+		}
 
-        if ($method === 'POST') {
-            if ($input = file_get_contents('php://input')) {
-                $jsonData = json_decode($input, true);
-                if (json_last_error() === JSON_ERROR_NONE) {
-                    $_POST = array_merge($_POST, $jsonData);
-                }
-            }
-            self::$request = $_POST;
-        } else {
-            self::$request = $_GET;
-        }
+		if ($method === 'POST') {
+			if ($input = file_get_contents('php://input')) {
+				$jsonData = json_decode($input, true);
+				if (json_last_error() === JSON_ERROR_NONE) {
+					$_POST = array_merge($_POST, $jsonData);
+				}
+			}
+			self::$request = $_POST;
+		} else {
+			self::$request = $_GET;
+		}
 
-        foreach (self::$routes[$method] as $route => $callback) {
-            if (strpos($route, '{')) {
-                $pattern = '#^' . preg_replace('/\{[a-zA-Z0-9_]+\}/', '([a-zA-Z0-9_]+)', $route) . '$#';
-                if (preg_match($pattern, $uri, $matches)) {
-                    array_shift($matches);
-                    if (is_callable($callback)) {
-                        $response = $callback(...$matches);
-                    } else if (is_array($callback) || is_object($callback)) {
-                        $controller = new $callback[0](self::$request);
-                        $response = $controller->{$callback[1]}(...$matches);
-                    }
-                    self::Response($response);
-                    return;
-                }
-            }
+		foreach (self::$routes[$method] as $route => $callback) {
+			if (strpos($route, '{')) {
+				$pattern = '#^' . preg_replace('/\{[a-zA-Z0-9_]+\}/', '([a-zA-Z0-9_]+)', $route) . '$#';
+				if (preg_match($pattern, $uri, $matches)) {
+					array_shift($matches);
+					if (is_callable($callback)) {
+						$response = $callback(...$matches);
+					} else if (is_array($callback) || is_object($callback)) {
+						$controller = new $callback[0](self::$request);
+						$response = $controller->{$callback[1]}(...$matches);
+					}
+					self::Response($response);
+					return;
+				}
+			}
 
-            if ($uri == $route) {
-                if (is_callable($callback)) {
-                    $response = $callback();
-                } else if (is_array($callback) || is_object($callback)) {
-                    $controller = new $callback[0](self::$request);
-                    $response = $controller->{$callback[1]}();
-                }
-                self::Response($response);
-                return;
-            }
-        }
+			if ($uri == $route) {
+				if (is_callable($callback)) {
+					$response = $callback();
+				} else if (is_array($callback) || is_object($callback)) {
+					$controller = new $callback[0](self::$request);
+					$response = $controller->{$callback[1]}();
+				}
+				self::Response($response);
+				return;
+			}
+		}
 
-        http_response_code(404);
-        if (!file_exists('../resources/views/404.php')) {
-            self::Response(['message' => '404 - Not Found']);
-            return;
-        }
+		http_response_code(404);
+		if (!file_exists('../resources/views/404.php')) {
+			self::Response(['message' => '404 - Not Found']);
+			return;
+		}
 
-        ob_start();
-        include '../resources/views/404.php';
-        $content = ob_get_clean();
-        echo $content;
-    }
+		ob_start();
+		include '../resources/views/404.php';
+		$content = ob_get_clean();
+		echo $content;
+	}
 
-    /*
+	/*
      * * Redirect to a different URL
      * @param string $url The URL to redirect to
      * @param int $statusCode The HTTP status code (default is 302)
      */
-    public static function redirect($url, $statusCode = 302): void
-    {
-        header("Location: $url", true, $statusCode);
-    }
+	public static function redirect($url, $statusCode = 302): void
+	{
+		header("Location: $url", true, $statusCode);
+	}
 }
