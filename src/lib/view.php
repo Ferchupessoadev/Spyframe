b36e42bd91801195cd13188193317b17eb090af4
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-10-18 14:28:54 -0300

    changed method view for render in the view

--- src/lib/view.php
+++ src/lib/view.php
@@ -21,29 +21,29 @@ namespace Spyframe\lib;
  */
 class view
 {
-    /**
-     * Render a view
-     * @param string $view
-     * @param array $data
-     * @return string
-     */
-    public function view(string $view, array $data = []): string
-    {
-        $view = str_replace('.', '/', $view);
+	/**
+	 * Render a view
+	 * @param string $view
+	 * @param array $data
+	 * @return string
+	 */
+	public static function render(string $view, array $data = []): string
+	{
+		$view = str_replace('.', '/', $view);
 
-        foreach ($data as $key => $value) {
-            $data[$key] = htmlspecialchars($value);
-        }
+		foreach ($data as $key => $value) {
+			$data[$key] = htmlspecialchars($value);
+		}
 
-        extract($data);
+		extract($data);
 
-        if (file_exists('../resources/views/' . $view . '.php')) {
-            ob_start();
-            include '../resources/views/' . $view . '.php';
-            $content = ob_get_clean();
-            return $content;
-        } else {
-            return 'View ' . $view . ' not found';
-        }
-    }
+		if (file_exists('../resources/views/' . $view . '.php')) {
+			ob_start();
+			include '../resources/views/' . $view . '.php';
+			$content = ob_get_clean();
+			return $content;
+		} else {
+			return 'View ' . $view . ' not found';
+		}
+	}
 }
